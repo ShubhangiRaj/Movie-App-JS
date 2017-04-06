6d893c6e4a17e4216415171977ea7d8a15b881f1
Shubhangi <shubhangi@teewe.in>
2017-04-06 13:51:50 +0530

    adding sort and next functionality

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 $(document).ready(function(){
 
-	$("#discoverMovies").click(function(){
+
+	$.getJSON({
+		url: "https://api.themoviedb.org/3/discover/movie?api_key=466839c4d8544152a58da0ad13d38545&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+	}).done(function(data){
+		var info = show(data);
+		$("#displayData").html(info);
+	});
+
+
+	 $("#sortMovies").click(function(){
 
 		$.getJSON({
-			url: "https://api.themoviedb.org/3/discover/movie?api_key=466839c4d8544152a58da0ad13d38545&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+			url: "https://api.themoviedb.org/3/discover/movie?api_key=466839c4d8544152a58da0ad13d38545&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1"
+		}).done(function(data){
+			var info = show(data);
+			$("#displayData").html(info);
+		})
+	});
+
+	$("genresMovies").click(function(){
+		$.getJSON({
+			url: "https://api.themoviedb.org/3/genre/movie/list?api_key=466839c4d8544152a58da0ad13d38545&language=en-US"
+		}).done(function(data){
+			var info = showGenres(data);
+			$("#displayGenres").html(info);
+		})
+	});
+
+	 $("#nextPage").click(function(){
+
+		$.getJSON({
+			url: "https://api.themoviedb.org/3/discover/movie?api_key=466839c4d8544152a58da0ad13d38545&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2"
 		}).done(function(data){
 			var info = show(data);
 			$("#displayData").html(info);
@@ -16,9 +44,20 @@ function show(data){
 	var results = data.results;
 	var showing = "";
 	for(var i = 0; i < results.length; i++){
-		showing+="<p>" + data.results[i].original_title + "</p>" ;
+		showing+="<p>" + data.results[i].original_title +"</p>" ;
+	}
+	return showing; 
+}
+
+function showGenres(data){
+	console.log(data);
+	var results = data.genres;
+	var showing = "";
+	for(var i = 0; i < genres.length; i++){
+		showing+="<p>" + data.genres[i].name +"</p>" ;
 	}
 	return showing; 
 }
 
 // 466839c4d8544152a58da0ad13d38545
+
